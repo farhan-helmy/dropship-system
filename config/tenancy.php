3cb2d7937f54cd7ca2e3044df269853e09203ca9
Ubuntu <ubuntu@ip-172-31-43-209.ap-southeast-1.compute.internal>
2021-08-10 07:59:27 +0000

    change to dev

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,8 +17,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        '127.0.0.1',
-        'localhost',
+        '52.221.243.187'
     ],
 
     /**
