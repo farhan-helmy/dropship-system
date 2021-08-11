244916ad95db7bf8287135e02ce0e0734c1225a6
Ubuntu <ubuntu@ip-172-31-43-209.ap-southeast-1.compute.internal>
2021-08-11 03:16:10 +0000

    deploy bash

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# activate maintenance mode
+sudo php artisan down
+# update source code
+git pull origin master
+# update PHP dependencies
+composer install --no-interaction --no-dev --prefer-dist
+# --no-interaction Do not ask any interactive question
+# --no-dev  Disables installation of require-dev packages.
+# --prefer-dist  Forces installation from package dist even for dev versions.
+# update database
+php artisan migrate --force
+# --force  Required to run when in production.
+# stop maintenance mode
+sudo php artisan up
