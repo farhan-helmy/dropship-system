06230c7eb75539a716885256bb8d26949191e331
farhan <farhanhlmy@gmail.com>
2021-09-02 19:59:56 +0800

    update header

--- resources/views/main/landing/index.blade.php
+++ resources/views/main/landing/index.blade.php
@@ -60,7 +60,7 @@
         <div class="container d-flex align-items-center justify-content-between">
 
             <div class="logo">
-                <h1><a href="penguindropship.com">PenguinDropship</a></h1>
+                <h1><a href="#">PenguinDropship</a></h1>
                 <!-- Uncomment below if you prefer to use an image logo -->
                 <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
             </div>
